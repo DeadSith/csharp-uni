5f5ed330a289954678b8105ddea94cd1b24484b9
Andrii Vasylyk <deadsith@hotmail.com>
2018-11-18 12:36:31 +0200

    Added Lab 7

---
+++ Lab1/Lab7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter directory to calculate size: ");
+            var directory = Console.ReadLine();
+            var size = await GetDirectorySize(directory);
+            Console.WriteLine($"Total size: {size} bytes");
+            Console.ReadKey();
+        }
+
+        static async Task<long> GetDirectorySize(string path)
+        {
+            try
+            {
+                Console.WriteLine($"Reading directory: {path}");
+                var files = Directory.GetFiles(path);
+                var fileSize = files.Sum(f => new FileInfo(f).Length);
+                var directories = Directory.GetDirectories(path);
+                var results = await Task.WhenAll(directories.Select(GetDirectorySize));
+                var size = results.Sum() + fileSize;
+                return size;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return 0;
+            }
+            
+        }
+    }
+}
